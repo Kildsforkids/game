84230fe22315c46b00fa22f4a66c8fbdcd4755e5
Foxclip <destroyApp2@gmail.com>
2020-01-23 05:01:53 +1000

    Пауза теперь работает

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,6 +4,8 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour
 {
     [SerializeField]
+    private GameObject frog;
+    [SerializeField]
     private Transform map, background;
     [SerializeField]
     private GameObject score;
@@ -32,27 +34,39 @@ public class GameController : MonoBehaviour
     {
         if (!isPaused)
         {
+            Debug.Log("PAUSE");
             map.GetComponent<MapController>().enabled = false;
             map.GetComponent<GroundMove>().enabled = false;
             background.GetComponent<GroundMove>().enabled = false;
             score.GetComponent<ScoreController>().enabled = false;
             Camera.main.GetComponent<CameraController>().enabled = false;
+            frog.GetComponent<Move>().enabled = false;
+            //frog.GetComponent<Rigidbody2D>().isKinematic = true;
+            //Physics.autoSimulation = false;
+            Time.timeScale = 0;
+            frog.GetComponent<FrogController>().enabled = false;
             Camera.main.GetComponent<AudioSource>().Stop();
             pausePanel.SetActive(true);
             pauseBtn.SetActive(false);
-            isPaused = false;
+            isPaused = true;
         }
         else
         {
+            Debug.Log("UNPAUSE");
             map.GetComponent<MapController>().enabled = true;
             map.GetComponent<GroundMove>().enabled = true;
             background.GetComponent<GroundMove>().enabled = true;
             score.GetComponent<ScoreController>().enabled = true;
             Camera.main.GetComponent<CameraController>().enabled = true;
+            frog.GetComponent<Move>().enabled = true;
+            //frog.GetComponent<Rigidbody2D>().isKinematic = false;
+            //Physics.autoSimulation = true;
+            Time.timeScale = 1;
+            frog.GetComponent<FrogController>().enabled = true;
             Camera.main.GetComponent<AudioSource>().Play();
             pausePanel.SetActive(false);
             pauseBtn.SetActive(true);
-            isPaused = true;
+            isPaused = false;
         }
     }
 
